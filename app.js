35e18d0e9f628e1d86021f9a96f31a0636f60cf7
Asif Asif <130058986+asifhameed762@users.noreply.github.com>
2024-03-04 19:58:31 +0500

    chapter 3

---
+++ app.js
@@ -0,0 +1,19 @@
+//Q1.
+var age;
+age = 30;
+alert("I am " + age + " years old")
+
+//Q2.
+var visit = 14 ;
+alert("You have visited this site " + visit + " times");
+
+//Q3.
+var birthYear = 1994 ;
+document.write(" Q3. My Birth Year is " + birthYear);
+document.write("<br/> Data type of my declared variable is number ");
+
+//Q4.   
+var visitorName = "Asif";
+var Product = "T-Shirt's";
+var Quantity = 5;
+document.write("<br/><br/> Q4." + visitorName + " ordered "  + Quantity + "  " + Product + " on XYZ Clothing Store.");
